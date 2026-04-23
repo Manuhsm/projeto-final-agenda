af9a3d7f3af8adef74553e95b50170ffa5198ff0
Manuhsm <manuelahsm167@gmail.com>
2026-04-22 22:03:31 -0300

    Começando o projeto

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Principal from './componentes/Principal/Principal';
+
+function App () {
+  return (
+    <>
+        <Principal />
+    </>
+  );
+}
+
+export default App
---
+++ src/componentes/Principal/Principal.jsx
@@ -0,0 +1,12 @@
+import "./Principal.css";
+
+function Principal() {
+  return (
+    <div>
+      <h2>Principal</h2>
+      <p>Bem-vindo à agenda da Manu!</p>
+    </div>
+  );
+}
+
+export default Principal;
